d87c890e34edd0a1cb7c7c603d9a0c3c88360220
Lewis Muthaura <lewislemmu10@gmail.com>
2023-06-11 22:49:34 +0300

    topNav

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-function TopNav() {
+import { Link } from "react-router-dom";
+import profileImg from "../../assets/images/profile-02.jpg";
+import "./top-nav.css";
+
+const TopNav = () => {
   return (
-    <div>TopNav</div>
+    <div className='top_nav'>
+      <div className="top_nav-wrapper">
+        <div className="search_box">
+          <input type="text" placeholder='search or type' />
+          <span>
+            <i class="ri-search-line"></i>
+            </span>
+        </div>
+        <div className="top_nav-right">
+          <span className='notification'><i 
+          class="ri-notification-3-line"></i>
+          <span className='badge'>1</span>
+          </span>
+          <div className="profile">
+            <Link to='/settings'><img src={profileImg} alt="" /></Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default TopNav
+export default TopNav;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  return <div className="dashboard">
+    <div className="dashboard_wrapper">
+      <div className="dashboard_cards">
+        <div className="single_card">
+          <div className="card_content">
+            <h4>Total Cases</h4>
+            <span>99+</span>
+          </div>
 
-export default Dashboard
+          <span><i class="ri-functions"></i></span>
+        </div>
+      </div>
+    </div>
+  </div>
+};
+
+export default Dashboard;
